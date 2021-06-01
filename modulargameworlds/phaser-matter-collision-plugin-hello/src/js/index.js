fb0aebcd4b1af9fd67cdf4b92aabb3937cb088ff
KristianRykkje <kriryk@gmail.com>
2021-06-01 20:29:22 +0200

    added repo and phaser-matter-collision-plugin hello

---
+++ modulargameworlds/phaser-matter-collision-plugin-hello/src/js/index.js
@@ -0,0 +1,24 @@
+import Phaser from "phaser";
+import PhaserMatterCollisionPlugin from "phaser-matter-collision-plugin";
+import MainScene from "./main-scene.js";
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: "#000c1f",
+  parent: "game-container",
+  scene: MainScene,
+  physics: { default: "matter" },
+  plugins: {
+    scene: [
+      {
+        plugin: PhaserMatterCollisionPlugin, // The plugin class
+        key: "matterCollision", // Where to store in Scene.Systems, e.g. scene.sys.matterCollision
+        mapping: "matterCollision", // Where to store in the Scene, e.g. scene.matterCollision
+      },
+    ],
+  },
+};
+
+const game = new Phaser.Game(config);
